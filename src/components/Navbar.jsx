e6d6c9e96d1d9c72a9538b7a0a9b9ac3398d96b0
sushant bhau <sushantbhau9541653458@gmail.com>
2024-12-24 10:51:14 +0530

    hh

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Logo from "../assets/Logo.jpg";
+import Logo from "../assets/logo.jpg";
 import { FaChevronRight } from "react-icons/fa";
 import { HiMenuAlt3, HiMenuAlt1 } from "react-icons/hi";
 import ResponsiveMenu from "./ResponsiveMenu";
